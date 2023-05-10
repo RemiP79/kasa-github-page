d8c47e82e427bdc841a21bfaf9e2edc79749229a
Remi P <perez.remi@gmail.com>
2023-05-10 14:55:10 +0200

    essai

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -6,11 +6,12 @@ import Img404 from "../assets/img/img404.png"
 function Page404() {
     return (
         <div className="divPage404">
-            <img src={Img404} alt="404" className="Img404"/>
+            <Navigate to="/"/>
+            {/* <img src={Img404} alt="404" className="Img404"/>
             <p className="p404">Oups ! La page que vous demandez n'existe pas.</p>
             <p className="link404">
                 <Link to= "/" className="navLink404">Retourner sur la page d'accueil</Link>    
-            </p>
+            </p> */}
         </div>
     )
 }
